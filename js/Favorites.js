08e97e075569200fdc23c82307c210c5a58c6595
alan cavalcante <alancavalcante307@gmail.com>
2023-06-05 22:11:02 -0300

    update

--- js/Favorites.js
+++ js/Favorites.js
@@ -19,29 +19,11 @@ class Favorites {
     this.tbody = this.root.querySelector("table tbody");
     this.load();
   }
+  userAlreadyExists(username) {
+    return this.users.some((user) => username === user.login);
+  }
 
   load() {
-    // this.users = [
-    //     {
-    //     login: 'alan589',
-    //     name: 'Alan Cavalcante',
-    //     public_repos: '55',
-    //     followers: '1000'
-    //     },
-    //     {
-    //     login: 'maykbrito',
-    //     name: 'AJulio cesar',
-    //     public_repos: '222',
-    //     followers: '123000'
-    //     },
-    //     {
-    //     login: 'allan',
-    //     name: 'Joao Fabio',
-    //     public_repos: '515',
-    //     followers: '11000'
-    //     }
-    // ]
-
     this.users = JSON.parse(localStorage.getItem("@github-users:")) || [];
     console.log(this.users);
   }
@@ -52,49 +34,136 @@ class Favorites {
     );
     this.users = filteredUsers;
     this.update();
+    this.save();
   }
 
-  add(username) {
-    GithubUser.search(username).then((user) => {
-      this.users.push(user)
-      this.update()
-    });
+  save() {
+    localStorage.setItem("@github-users:", JSON.stringify(this.users));
+  }
+
+  async add(username) {
+    try {
+      if (this.userAlreadyExists(username))
+        throw new Error("User already exists");
+
+      const user = await GithubUser.search(username);
+      console.log(user);
+      if (user.login === undefined) throw new Error("User not found");
+      // ANOTAR SPREAD OPERATOR E O AWAIT
+      this.users = [...this.users, user];
+      this.update();
+      this.save();
+    } catch (error) {
+      alert(error.message);
+    }
   }
 }
 
 export class FavoritesView extends Favorites {
+  itemsPerPage = 5;
+  currentPage = 1;
+
   constructor(root) {
     super(root);
     this.update();
   }
 
+  displayUsers() {
+    let startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    let endIndex = startIndex + this.itemsPerPage;
+    let paginatedItems = this.users.slice(startIndex, endIndex);
+
+    paginatedItems.forEach((user) => {
+      const tr = this.tbody.appendChild(this.createTableRow(user));
+      tr.querySelector(".remove").onclick = () => {
+        const isOk = confirm("Deletar?");
+        if (isOk) {
+          this.delete(user);
+        }
+      };
+    });
+
+    this.displayPagination();
+  }
+
+  displayPagination() {
+    let totalPages = Math.ceil(this.users.length / this.itemsPerPage);
+
+    let tr = document.createElement("tr");
+    let td = document.createElement("td");
+
+    td.innerHTML = "";
+
+    let previousPage = document.createElement("span");
+    let nextPage = document.createElement("span");
+    let currentPage = document.createElement("span");
+
+    previousPage.textContent = "Prev";
+    nextPage.textContent = "Next";
+    currentPage.textContent = this.currentPage;
+
+    nextPage.classList.add("page-link");
+    previousPage.classList.add("page-link");
+    currentPage.classList.add("page-link");
+
+    nextPage.addEventListener("click", () => {
+      this.currentPage += 1;
+      if(this.currentPage <= totalPages) {
+        this.update()
+      }else {
+        this.currentPage = totalPages
+      }
+    });
+
+    previousPage.addEventListener("click", () => {
+      this.currentPage -= 1;
+      if(this.currentPage >= 1) {
+        this.update()
+      }
+      else {
+        this.currentPage = 1
+      }
+    });
+
+    td.appendChild(previousPage);
+    td.appendChild(currentPage);
+    td.appendChild(nextPage);
+    tr.appendChild(td);
+    td.setAttribute("colspan", "4");
+    td.style.textAlign = "center";
+    this.tbody.appendChild(tr);
+  }
+
+  onAdd() {
+    const addButton = this.root.querySelector(".search button");
+    addButton.onclick = (e) => {
+      e.preventDefault();
+      const { value } = this.root.querySelector(".search input");
+      this.add(value);
+    };
+  }
+
   update() {
     this.removeAllTableRow();
-    
+    console.log(this.currentPage)
     if (this.users.length !== 0) {
-      this.users.forEach((user) => {
-        const tr = this.tbody.appendChild(this.createTableRow(user));
-        tr.querySelector(".remove").onclick = () => {
-          const isOk = confirm("Deletar?");
-          if (isOk) {
-            this.delete(user);
-          }
-        };
-      });
+      this.displayUsers();
     } else {
-        const tr = document.createElement("tr");
-        tr.innerHTML = '<td colspan="4" style="text-align:center;">No user</td>';
-        this.tbody.appendChild(tr);
-      }
+      const tr = document.createElement("tr");
+      tr.innerHTML = '<td colspan="4" style="text-align:center;">No user</td>';
+      this.tbody.appendChild(tr);
+    }
   }
 
   createTableRow(user) {
     const tr = document.createElement("tr");
     tr.innerHTML = `
             <td class="user">
-                <img src="https://www.github.com/${user.login}.png" alt="${user.login}'s photo">
+                <img src="https://www.github.com/${user.login}.png" alt="${
+      user.login
+    }'s photo">
                 <a href="https://www.github.com/${user.login}" target=_blank>
-                    <p>${user.name}</p>
+                    <p>${user.name || ""}</p>
                     <span>${user.login}</span>
                 </a>
             </td>
--- js/main.js
+++ js/main.js
@@ -3,10 +3,4 @@ import { FavoritesView, GithubUser } from "./Favorites.js";
 
 
 const favoritesView = new FavoritesView('#app')
-
-
-document.querySelector('.search button').onclick = (e) => {
-    e.preventDefault()
-    favoritesView.add(document.querySelector('#input-search').value)
-}
-
+favoritesView.onAdd()
